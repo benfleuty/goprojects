b90d1c2200676a96a7d658a88b1adf03657e2135
Ben Fleuty <bfleuty@outlook.com>
2025-03-07 18:29:59 +0000

    Remove description flag

--- 01-todo-list/cmd/add.go
+++ 01-todo-list/cmd/add.go
@@ -17,22 +17,9 @@ var addCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("add called")
 		fmt.Println(args)
-		parseDesc()
 	},
 }
 
-func parseDesc() {
-	if description == "" {
-		fmt.Println("You need to provide a description for your task!")
-		return
-	}
-
-	fmt.Println("Added task: \" " + description + "\".")
-}
-
-var description string
-
 func init() {
 	rootCmd.AddCommand(addCmd)
-	addCmd.Flags().StringVarP(&description, "description", "d", "", "A description of your task.")
 }
