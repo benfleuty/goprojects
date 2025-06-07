cf3f83918ed65340c81f5be679fe952d7eb9c3b2
Ben Fleuty <bfleuty@outlook.com>
2025-06-07 22:27:33 +0100

    Add result of add to response for /add
    - Add SimpleCalculationRequest and SimpleCalculationResponse
    - Response with result of number1 + number2
    - Add UUIDs for each request for ease of logging
    - Mark CalculationResult deprecated. Will be removed when other
      endpoints are updated.

--- 2-backend-api/handlers/handlers.go
+++ 2-backend-api/handlers/handlers.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/benfleuty/goprojects/calcapi/constants"
+	"github.com/google/uuid"
 )
 
 func SetResponse(w http.ResponseWriter, status int, data any) {
@@ -19,26 +20,45 @@ func SetResponse(w http.ResponseWriter, status int, data any) {
 	}
 }
 
+// Deprecated: Use SimpleCalculationRequest
 type CalculationResult struct {
 	Operation string `json:"operation"`
 }
 
+type SimpleCalculationResponse struct {
+	Result int `json:"result"`
+}
+
+type SimpleCalculationRequest struct {
+	Number1 int `json:"number1"`
+	Number2 int `json:"number2"`
+}
+
 func GetAdd(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+	sUuid := uuid.NewString()
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
-		msg := fmt.Sprintf("%s %s Error reading request body: %v", r.Method, r.URL.Path, err)
+		msg := fmt.Sprintf("%s %s %s Error reading request body: %v", sUuid, r.Method, r.URL.Path, err)
 		slog.Error(msg)
-		http.Error(w, msg, 400)
+		http.Error(w, msg, 500)
 		return
 	}
 
-	slog.Info(fmt.Sprintf("%s %s request with body: %s", r.Method, r.URL.Path, bodyBytes))
+	slog.Info(fmt.Sprintf("%s %s %s request with body: %s", sUuid, r.Method, r.URL.Path, bodyBytes))
+	body := &SimpleCalculationRequest{}
+	if err := json.Unmarshal(bodyBytes, &body); err != nil {
+		msg := fmt.Sprintf("%s Error parsing body!", sUuid)
+		slog.Error(msg)
+		http.Error(w, msg, 400)
+	}
 
-	var data []CalculationResult
-	obj := CalculationResult{}
-	data = append(data, obj)
-	SetResponse(w, 200, data)
+	responseBody := &SimpleCalculationResponse{}
+	responseBody.Result = body.Number1 + body.Number2
+
+	slog.Info(fmt.Sprintf("%s Sending response 200 OK with body: %+v", sUuid, responseBody))
+
+	SetResponse(w, 200, responseBody)
 }
 
 func GetSubtract(w http.ResponseWriter, r *http.Request) {
